8846c06501706adf0c937826b02621987027a07b
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2023-12-12 16:45:28 -0300

    Create Selecionando colunas especificas.sql

---
+++ Aulas/Consultando dados utilizando o comando SQL SELECT/Selecionando colunas especificas.sql
@@ -0,0 +1,20 @@
+SELECT
+    department_id,
+    location_id
+FROM
+    departments;
+
+SELECT
+    employee_id,
+    first_name,
+    last_name,
+    salary
+FROM
+    employees;
+
+SELECT
+    department_id,
+    department_name,
+    manager_id
+FROM
+    departments;
