b72a9a1a5ec10d2d8cc90b6d61760c71ff273bef
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2023-12-12 16:45:30 -0300

    Create Utilizando o comando DESCRIBE.sql

---
+++ Aulas/Consultando dados utilizando o comando SQL SELECT/Utilizando o comando DESCRIBE.sql
@@ -0,0 +1,24 @@
+describe employees;
+
+desc employees;
+
+desc departments;
+
+desc jobs;
+
+desc locations;
+
+SELECT
+    *
+FROM
+    employees;
+
+SELECT
+    *
+FROM
+    jobs;
+
+SELECT
+    *
+FROM
+    departments;
