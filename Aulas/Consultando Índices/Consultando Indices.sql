d3b882b9782c2f9d719029b58d0ec8570c4ff15c
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2023-12-26 17:45:20 -0300

    Atualização

--- Aulas/Consultando Índices/Consultando Indices.sql
+++ Aulas/Consultando Índices/Consultando Indices.sql
@@ -7,11 +7,11 @@ SELECT
     ix.status,
     ix.tablespace_name
 FROM
-         all_indexes ix
-    JOIN all_ind_columns ic ON ( ix.index_name = ic.index_name )
+         user_indexes ix
+    JOIN user_ind_columns ic ON ( ix.index_name = ic.index_name )
                                 AND ( ix.table_name = ic.table_name )
 WHERE
-    ix.table_name = 'EMPLOYEES'
+    ix.table_name = 'PROJECTS'
 ORDER BY
     ix.index_name,
     ic.column_position;
