7343a7a986df915fa0aa3a6b0d7a9975832b29ca
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2023-12-26 16:46:11 -0300

    Atualização

---
+++ Aulas/Consultando Índices/Consultando Indices.sql
@@ -0,0 +1,17 @@
+SELECT
+    ix.index_name,
+    ic.column_name,
+    ic.column_position,
+    ix.index_type,
+    ix.uniqueness,
+    ix.status,
+    ix.tablespace_name
+FROM
+         user_indexes ix
+    JOIN user_ind_columns ic ON ( ix.index_name = ic.index_name )
+                                AND ( ix.table_name = ic.table_name )
+WHERE
+    ix.table_name = 'EMPLOYEES'
+ORDER BY
+    ix.index_name,
+    ic.column_position;
