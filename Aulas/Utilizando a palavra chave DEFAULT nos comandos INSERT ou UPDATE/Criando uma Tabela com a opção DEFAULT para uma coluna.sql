5df9190f817fcb08010a9d8afd2113b21740e71e
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-01-02 16:07:35 -0300

    Atualização

---
+++ Aulas/Utilizando a palavra chave DEFAULT nos comandos INSERT ou UPDATE/Criando uma Tabela com a opção DEFAULT para uma coluna.sql
@@ -0,0 +1,14 @@
+DROP TABLE projects;
+
+CREATE TABLE projects (
+    project_id    NUMBER(6) NOT NULL,
+    project_code  VARCHAR2(10) NOT NULL,
+    project_name  VARCHAR2(100) NOT NULL,
+    creation_date DATE DEFAULT sysdate NOT NULL,
+    start_date    DATE,
+    end_date      DATE,
+    status        VARCHAR2(20) NOT NULL,
+    priority      VARCHAR2(10) NOT NULL,
+    budget        NUMBER(11, 2) NOT NULL,
+    description   VARCHAR2(400) NOT NULL
+);
---
+++ Aulas/Utilizando a palavra chave DEFAULT nos comandos INSERT ou UPDATE/Utilizando a palavra chave (keyword) DEFAULT para o valor de uma coluna.sql
@@ -0,0 +1,20 @@
+INSERT INTO projects (
+    project_id,
+    project_code,
+    project_name,
+    creation_date,
+    status,
+    priority,
+    budget,
+    description
+) VALUES (
+    1,
+    'PROJERP001',
+    'Oracle ERP', DEFAULT,
+    'PLANNIG',
+    'HIGH',
+    1000000,
+    'Implantação do Oracle ERP'
+);
+
+COMMIT;
