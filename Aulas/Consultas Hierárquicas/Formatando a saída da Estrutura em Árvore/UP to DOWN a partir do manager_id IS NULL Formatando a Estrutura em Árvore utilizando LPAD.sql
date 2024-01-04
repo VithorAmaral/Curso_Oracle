8845fc4487f1c94770fc7509231d4fa9aca07309
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-01-04 16:14:24 -0300

    Atualização

---
+++ Aulas/Consultas Hierárquicas/Formatando a saída da Estrutura em Árvore/UP to DOWN a partir do manager_id IS NULL Formatando a Estrutura em Árvore utilizando LPAD.sql
@@ -0,0 +1,26 @@
+SELECT
+    level,
+    lpad(last_name,
+         length(last_name) +(level * 4) - 4,
+         ' ')
+FROM
+    employees
+START WITH
+    manager_id IS NULL
+CONNECT BY
+    PRIOR employee_id = manager_id;
+
+SELECT
+    lpad(level
+         || '.'
+         || last_name,
+         length(level
+                || '.'
+                || last_name) +(level * 4) - 4,
+         ' ')
+FROM
+    employees
+START WITH
+    manager_id IS NULL
+CONNECT BY
+    PRIOR employee_id = manager_id;
