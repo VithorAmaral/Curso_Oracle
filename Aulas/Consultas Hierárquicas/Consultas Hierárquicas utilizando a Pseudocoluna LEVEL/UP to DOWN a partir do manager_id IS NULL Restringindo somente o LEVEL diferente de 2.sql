bba67b1990ec99ecc0eaa740d75808e42a2312b0
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-01-04 15:30:30 -0300

    Atualização

---
+++ Aulas/Consultas Hierárquicas/Consultas Hierárquicas utilizando a Pseudocoluna LEVEL/UP to DOWN a partir do manager_id IS NULL Restringindo somente o LEVEL diferente de 2.sql
@@ -0,0 +1,15 @@
+SELECT
+    level,
+    employee_id,
+    first_name,
+    last_name,
+    job_id,
+    manager_id
+FROM
+    employees
+WHERE
+    level <> 2
+START WITH
+    manager_id IS NULL
+CONNECT BY
+    PRIOR employee_id = manager_id;
