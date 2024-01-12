9e21ebf30e9f9328649ee576d6ea013784617583
Vithor Amaral <151647805+VithorAmaral@users.noreply.github.com>
2024-01-12 14:43:49 -0300

    Atualização

---
+++ Aulas/Globalização Tratamento de Datas e Time Zones/Globalização - Time Zones/Consultando Time Zones.sql
@@ -0,0 +1,20 @@
+SELECT
+    *
+FROM
+    v$timezone_names
+WHERE
+    tzname LIKE '%Sao_Paulo%';
+
+SELECT
+    *
+FROM
+    v$timezone_names
+WHERE
+    tzname LIKE '%Brazil%';
+
+SELECT
+    *
+FROM
+    v$timezone_names
+WHERE
+    tzname LIKE '%New_York%';
